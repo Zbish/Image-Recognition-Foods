be2b7bd16a0518acaa43d7ae45828c5b13704051
Omri Doron <omridoron3@gmail.com>
2017-12-21 17:54:42 +0200

    clarifai

--- App.js
+++ App.js
@@ -1,8 +1,3 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
 
 import React, { Component } from 'react';
 import {
@@ -11,15 +6,31 @@ import {
   Text,
   View
 } from 'react-native';
-
+import Clarifai from 'clarifai';
+import hug from './src/images/field.jpg'
+process.nextTick = setImmediate
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' +
     'Cmd+D or shake for dev menu',
-  android: 'Kimaia Image recognition',
+  android: 'Kimaia Image recognition fff',
 });
 
+const app = new Clarifai.App({
+ apiKey: 'b5bfcb7aba854f9da7b6d6e418d778cb'
+});
+ function gettext(){
+  app.models.predict(Clarifai.GENERAL_MODEL, 'http://ptorigin.parenttoolkit.com/images/dmImage/ImageShare/Share_1200x900_tablefood.png').then(
+    function(response) {
+      console.log('response' ,response);
+    },
+    function(err) {
+      console.error(err);
+    }
+  );
+}
 export default class App extends Component {
   render() {
+   gettext()
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
